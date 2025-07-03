0a2f179f2f744de6f307cc0b5cf2215159557403
just_cbrick <apazadaniel299@gmail.com>
2025-07-03 09:55:27 -0400

    Add files via upload

---
+++ Diseño grafico things/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [App],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class app {
+
+}
+import { Footer } from './footer/footer';
+import { App } from "./app";
